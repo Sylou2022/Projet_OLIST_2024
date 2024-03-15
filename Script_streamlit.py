b87fee337e946c134a392c7950d2e75061b0e95a
Sylou2022 <sylvaindiouf18@gmail.com>
2024-03-15 15:26:56 +0100

    t2

--- Script_streamlit.py
+++ Script_streamlit.py
@@ -6,7 +6,7 @@ import plotly.express as px
 # charger les données depuis un fichier CSV
 @st.cache_data
 def load_data(file_path):
-    return pd.read_csv(file_path, sep=';')
+    return pd.read_csv(file_path, sep=',')
 
 
 # afficher la segmentation des clients
